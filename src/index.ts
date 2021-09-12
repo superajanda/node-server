d83c14f0f3d6b8cc3a5676acf1a8a426595e1d86
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-09-13 00:06:21 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import { NODE_ENV } from "./constants";
+import express from "express";
+import morgan from "morgan";
+import { prisma } from "./lib/prisma";
+import { PORT } from "./constants";
+import { authMiddleware } from "./auth";
+import { router } from "./routes";
 
-console.log(`NODE_ENV: ${NODE_ENV}`);
+const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(authMiddleware);
+app.use(router);
+
+async function main() {
+  app.listen(PORT, () => {
+    console.log(`LISTENING IN PORT ${PORT}`);
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

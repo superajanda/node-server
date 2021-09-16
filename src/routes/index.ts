81bf792fd43783b6119fbbde699650c43c82a630
bayram-kazik <48856944+bayram-kazik@users.noreply.github.com>
2021-09-17 00:11:09 +0300

    Remove not working express error handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,9 @@ import express from "express";
 import { notesRouter } from "./notes";
 import { tagsRouter } from "./tags";
 import { usersRouter } from "./users";
-import { ERROR_MESSAGES } from "../constants";
 
 export const router = express.Router();
 
 router.use("/notes", notesRouter);
 router.use("/tags", tagsRouter);
 router.use("/users", usersRouter);
-router.use((_req, res) => {
-  res.status(404).json({ message: ERROR_MESSAGES.unknownRoute });
-});
